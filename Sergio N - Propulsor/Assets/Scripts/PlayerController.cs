517ffbcabb710bc790e14b7c1b8c735e351ae03a
David Bolívar <sergio13nb24@gmail.com>
2022-10-06 11:18:37 +0100

    Sergio N - Propulsor

--- Sergio N - Propulsor/Assets/Scripts/PlayerController.cs
+++ Sergio N - Propulsor/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,8 +11,20 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]  //para que se vea en unity, con el float
     float impulse = 2f;    // el impulso que quieres ponerle
+
+    [SerializeField]
+    TextMeshProUGUI labelFuel;
+
+
+    float gasolinaActual = 100f;
+
+    [SerializeField]
+    GameObject prefabParticles;
+
     void Start()
     {
+        gasolinaActual = 100f;
+
         body = GetComponent<Rigidbody2D>();
 
     }
@@ -25,6 +38,11 @@ public class PlayerController : MonoBehaviour
 
         //para que se mueva con wasd o joystick   // con esto creas la misma cantidad de impulso para todos
 
+        //para la gasolina
+        gasolinaActual = gasolinaActual - 0.5f * Time.deltaTime;
+        labelFuel.text = gasolinaActual.ToString("00") + " %";
+
+
     }
 
     private void FixedUpdate()
@@ -33,5 +51,47 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    //Crear un objeto que al colisionar con él sucedan dos cosas
+    //Añadir gasolina
+    //Destruir el objeto recogido -> Destroy / SetActive(false)
 
-} 
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.name == "gasolina")
+        {
+            gasolinaActual += 10f;
+            if (gasolinaActual > 100f)
+            {
+                gasolinaActual = 100f;
+            }
+            // este `if` para que no se pase de 100% la gasolina
+
+            
+            //crear particulas
+            Instantiate(prefabParticles, collision.transform.position, collision.transform.rotation);
+
+           
+            Destroy(collision.gameObject);
+            //destruirlo
+
+                 //desactivarlo sería con:
+                 //collision.gameObject.SetActive(false);
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
